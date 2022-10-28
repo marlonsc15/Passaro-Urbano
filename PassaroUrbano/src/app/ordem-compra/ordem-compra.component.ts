25a76d131f565f98b48afb43aa8919ccbfedc154
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-10-28 15:32:48 -0300

    certificação de input valido

--- PassaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
+++ PassaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
@@ -12,6 +12,16 @@ export class OrdemCompraComponent implements OnInit {
   public complemento: string = ''
   public formaPagamento: string = ''
 
+  public enderecoValido!: boolean
+  public numeroValido!: boolean
+  public complementoValido!: boolean
+  public formaPagamentoValido!: boolean
+
+  public enderecoEstadoPrimitivo: boolean = true
+  public numeroEstadoPrimitivo: boolean = true
+  public complementoEstadoPrimitivo: boolean = true
+  public formaPagamentoEstadoPrimitivo: boolean = true
+
   constructor() { }
 
   ngOnInit(): void {
@@ -19,18 +29,48 @@ export class OrdemCompraComponent implements OnInit {
 
   public atualizaEndereco(endereco: string): void {
     this.endereco = endereco
+
+    this.enderecoEstadoPrimitivo = false
+
+    if(this.endereco.length > 3) {
+      this.enderecoValido = true
+    }else {
+      this.enderecoValido = false
+    }
   }
 
   public atualizaNumero(numero: string): void {
     this.numero = numero
+
+    this.numeroEstadoPrimitivo = false
+
+    if(this.numero.length > 3) {
+      this.numeroValido = true
+    }else {
+      this.numeroValido = false
+    }
   }
 
   public atualizaComplemento(complemento: string): void {
     this.complemento = complemento
+
+    this.complementoEstadoPrimitivo = false
+
+    if(this.complemento.length > 0) {
+      this.complementoValido = true
+    }
   }
   
   public atualizaFormaPagamento(formaPagamento: string): void {
     this.formaPagamento = formaPagamento
+
+    this.formaPagamentoEstadoPrimitivo = false 
+
+    if(this.formaPagamento.length > 0) {
+      this.formaPagamentoValido = true
+    }else {
+      this.formaPagamentoValido = false
+    }
   }
 
 
