73ddfb1b82bacc0f20d8db04ac48bd9b25d7ad7a
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-11-07 16:11:55 -0300

    envio com quantidade de itens

--- PassaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
+++ PassaroUrbano/src/app/ordem-compra/ordem-compra.component.ts
@@ -3,61 +3,76 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { OrdemCompraService } from '../ordem-compra.service'
 import { Pedido } from '../shared/pedido.model'
 import { CarrinhoService } from '../carrinho.service'
+import { ItemCarrinho } from '../shared/item-carrinho.model';
 
 @Component({
   selector: 'app-ordem-compra',
   templateUrl: './ordem-compra.component.html',
   styleUrls: ['./ordem-compra.component.css'],
-  providers: [ OrdemCompraService ]
+  providers: [OrdemCompraService]
 })
 export class OrdemCompraComponent implements OnInit {
 
   public idPedidoCompra!: number
+  public itensCarrinho: ItemCarrinho[] = []
 
   public formulario: FormGroup = new FormGroup({
-    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120) ]),
-    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(10) ]),
+    'endereco': new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(120)]),
+    'numero': new FormControl(null, [Validators.required, Validators.minLength(1), Validators.maxLength(10)]),
     'complemento': new FormControl(null),
     'formaPagamento': new FormControl(null, [Validators.required]),
   })
 
   constructor(
     private ordemCompraService: OrdemCompraService,
-    private carrinhoService: CarrinhoService
-    ) { }
+    public carrinhoService: CarrinhoService
+  ) { }
 
   ngOnInit() {
-    console.log('Ordem compra - Array de itens do carrinho: ', this.carrinhoService.exibirItens());
-    
+    this.itensCarrinho = this.carrinhoService.exibirItens()
+    console.log(this.itensCarrinho);
+
   }
 
   public confirmarCompra(): void {
-    
-    if(this.formulario.status === 'INVALID'){
-      console.log('formulario esta inválido')
+
+    if (this.formulario.status === 'INVALID') {
 
       this.formulario.get('endereco')?.markAsTouched()
       this.formulario.get('numero')?.markAsTouched()
       this.formulario.get('complemento')?.markAsTouched()
       this.formulario.get('formaPagamento')?.markAsTouched()
-      
-    } else {
-      
-      let pedido: Pedido = new Pedido(
-        this.formulario.value.endereco,
-        this.formulario.value.numero,
-        this.formulario.value.complemento,
-        this.formulario.value.formaPagamento
-      )
-      
-      this.ordemCompraService.efetivarCompra(pedido)
-      .subscribe((idPedido: number) => {
-        this.idPedidoCompra = idPedido
-        console.log(this.idPedidoCompra);
-        
-      })
 
+    } else {
+
+      if (this.carrinhoService.exibirItens().length === 0) {
+        alert('Voce näo selecionou nenhum item!')
+      }else{
+
+        let pedido: Pedido = new Pedido(
+          this.formulario.value.endereco,
+          this.formulario.value.numero,
+          this.formulario.value.complemento,
+          this.formulario.value.formaPagamento,
+          this.carrinhoService.exibirItens()
+        )
+
+        this.ordemCompraService.efetivarCompra(pedido)
+          .subscribe((idPedido: number) => {
+            this.idPedidoCompra = idPedido
+            this.carrinhoService.limparCarrinho()
+
+          })
+      }
     }
-    
+
   }
+  public adicionar(item: ItemCarrinho): void {
+    this.carrinhoService.adicionarQuantidade(item)
+  }
+
+  public remover(item: ItemCarrinho): void {
+    this.carrinhoService.removerQuantidade(item)
+  }
+
 }
